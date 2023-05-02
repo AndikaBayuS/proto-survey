31043c7281a871a83cdfc93cf1c9977399ab37db
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2023-05-02 14:13:23 +0700

    feat: swr handling error

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -3,5 +3,13 @@ export default async function fetcher<JSON = any>(
   init?: RequestInit
 ): Promise<JSON> {
   const res = await fetch(input, init);
+  if (!res.ok) {
+    const error = new Error(
+      "An error occurred while fetching the data."
+    ) as any;
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
   return res.json();
 }
