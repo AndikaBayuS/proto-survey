a96dcbe870b6d4a1fc5d0cd6e524e263d4fe9f68
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2023-06-07 14:48:22 +0700

    experimental: check middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 
-export function middleware(req: NextRequest) {
-  const session = req.cookies.get("next-auth.session-token");
-  const prodSession = req.cookies.get("__Secure-next-auth.session-token");
-  if (!session || !prodSession) {
-    return NextResponse.rewrite(new URL("/auth/signin", req.nextUrl));
-  } else {
-    return NextResponse.rewrite(new URL(req.nextUrl));
-  }
-}
+export default withAuth({
+  callbacks: {
+    authorized({ req, token }) {
+      console.log("nectauth", req.cookies.has('next-auth.session-token'));
+      console.log("secure", req.cookies.has('__Secure-next-auth.session-token'));
+      return !!token;
+    },
+  },
+});
 
 export const config = {
   matcher: ["/survey/:path*", "/profile"],
