8ddfdd0339420f674d790b294f4bfc4b8163d8a9
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2023-06-07 11:44:19 +0700

    experimental: adjust middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
 
 export function middleware(req: NextRequest) {
-  const session = req.cookies.get("next-auth.session-token");
+  const session = req.cookies.get(
+    "next-auth.session-token" || "__Secure-next-auth.session-token"
+  );
   if (!session) {
     return NextResponse.rewrite(new URL("/auth/signin", req.nextUrl));
   } else {
