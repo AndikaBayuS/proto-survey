584e7925b4843928f2e0e7c97e6f1504a977cc44
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2023-06-08 09:57:49 +0700

    experimental: try to fix profile update

--- src/pages/api/profile/update/index.tsx
+++ src/pages/api/profile/update/index.tsx
@@ -2,15 +2,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 
 import { prisma } from "@/src/lib/prisma";
+import { getUserId } from "@/src/utils/prisma/user";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const session = await getSession({ req });
+  const userId = await getUserId(session?.user?.email!);
   await prisma.user.update({
     where: {
-      email: String(session?.user?.email),
+      id: userId,
     },
     data: {
       name: req.body.name,
